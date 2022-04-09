db2c7757491c4e0eb5d78f80929d2a72f2dce757
dhanyn10 <dhanyn.dhan@gmail.com>
2022-04-10 00:23:52 +0700

    tambah tabel transaksi

--- nasabah.sql
+++ nasabah.sql
@@ -1,5 +1,7 @@
 -- Adminer 4.8.1 PostgreSQL 14.2 (Debian 14.2-1.pgdg110+1) dump
 
+\connect "admin";
+
 DROP TABLE IF EXISTS "nasabah";
 DROP SEQUENCE IF EXISTS nasabah_id_seq;
 CREATE SEQUENCE nasabah_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
@@ -15,7 +17,21 @@ CREATE TABLE "public"."nasabah" (
 ) WITH (oids = false);
 
 INSERT INTO "nasabah" ("id", "username", "password", "token", "tabungan") VALUES
-(1,	'hero',	'hero',	'token',	10000),
-(2,	'van helsing',	'van helsing',	'token',	35000);
+(1,	'hero',	'hero',	'token',	150000000),
+(2,	'zeus',	'zeus',	'token',	35000000);
 
--- 2022-04-09 06:17:19.945812+00
+DROP TABLE IF EXISTS "transaksi";
+DROP SEQUENCE IF EXISTS transaksi_id_seq;
+CREATE SEQUENCE transaksi_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."transaksi" (
+    "id" integer DEFAULT nextval('transaksi_id_seq') NOT NULL,
+    "from" character varying(250) NOT NULL,
+    "to" character varying(250) NOT NULL,
+    "amount" money NOT NULL,
+    "datetime" date NOT NULL,
+    CONSTRAINT "transaksi_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+-- 2022-04-09 16:05:58.180909+00
